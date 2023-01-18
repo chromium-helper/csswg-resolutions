ae0057f74dc6c3b04b986b287b01bab736b7785f
Vladimir Levin <vmpstr@chromium.org>
2023-01-17 21:56:52 -0500

    Use gcp secret manager for github api key

--- csswg-to-local-cf/github-resolutions.go
+++ csswg-to-local-cf/github-resolutions.go
@@ -5,12 +5,46 @@ import (
   "github.com/google/go-github/v49/github"
   "fmt"
   "context"
+  gcpsm "cloud.google.com/go/secretmanager/apiv1"
+  gcpsmpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 )
+
+const (
+  gcpProject = "chromium-csswg-helper"
+  gcpGithubAPIKeySecret = "github-api-key"
+)
+
+func getGithubAPIToken(ctx context.Context) (string, error) {
+  client, err := gcpsm.NewClient(ctx)
+  if err != nil {
+    return "", fmt.Errorf("gcpsm.NewClient: %v", err)
+  }
+  defer client.Close()
+
+  req := &gcpsmpb.AccessSecretVersionRequest{
+    Name:
+      fmt.Sprintf("projects/%s/secrets/%s/versions/latest",
+        gcpProject,
+        gcpGithubAPIKeySecret,
+      ),
+  }
+
+  secret, err := client.AccessSecretVersion(ctx, req)
+  if err != nil {
+    return "", fmt.Errorf("gcpsm.AccessSecretVersion: %v\n", err)
+  }
+  return string(secret.Payload.GetData()), nil
+}
 
 func main() {
   ctx := context.Background()
+  token, err := getGithubAPIToken(ctx)
+  if err != nil {
+    panic(err)
+  }
+
   ts := oauth2.StaticTokenSource(
-    &oauth2.Token{AccessToken: "..."},
+    &oauth2.Token{AccessToken: token},
   )
   tc := oauth2.NewClient(ctx, ts)
 
@@ -24,6 +58,5 @@ func main() {
   for _, issue := range issues {
     fmt.Printf("%d: %s\n", *issue.Number, *issue.Title)
   }
-
 }
 
