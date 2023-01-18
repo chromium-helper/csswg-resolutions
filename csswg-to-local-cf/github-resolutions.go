678c1b8066a1b0320e37d40e62a156c0cd9eb154
Vladimir Levin <vmpstr@chromium.org>
2023-01-18 12:47:43 -0500

    Getting firestore values

--- csswg-to-local-cf/github-resolutions.go
+++ csswg-to-local-cf/github-resolutions.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-  _ "golang.org/x/oauth2"
+   "golang.org/x/oauth2"
   "github.com/google/go-github/v49/github"
   "fmt"
   "context"
+  "time"
+  "log"
   gcpsm "cloud.google.com/go/secretmanager/apiv1"
   gcpsmpb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+  gcpfs "cloud.google.com/go/firestore"
 )
 
 const (
   gcpProject = "chromium-csswg-helper"
   gcpGithubAPIKeySecret = "github-api-key"
+  gcpFirestoreCollection = "resolution-db"
 )
 
-func getGithubAPIToken(ctx context.Context) (string, error) {
-  client, err := gcpsm.NewClient(ctx)
+type App struct {
+  gh_client_ro *github.Client
+  gh_client_rw *github.Client
+  ctx context.Context
+  start_time time.Time
+}
+
+// Creates a new app to use
+func NewApp(ctx context.Context) *App {
+  return &App{
+    gh_client_ro: github.NewClient(nil),
+    gh_client_rw: nil,
+    ctx: ctx,
+    start_time: time.Now(),
+  }
+}
+
+// Retrieves the github api token from gcp secret manager. Don't use unless you
+// need write access to github.
+func (app *App) getGithubAPIToken() (string, error) {
+  client, err := gcpsm.NewClient(app.ctx)
   if err != nil {
     return "", fmt.Errorf("gcpsm.NewClient: %v", err)
   }
@@ -29,35 +52,75 @@ func getGithubAPIToken(ctx context.Context) (string, error) {
       ),
   }
 
-  secret, err := client.AccessSecretVersion(ctx, req)
+  secret, err := client.AccessSecretVersion(app.ctx, req)
   if err != nil {
     return "", fmt.Errorf("gcpsm.AccessSecretVersion: %v\n", err)
   }
   return string(secret.Payload.GetData()), nil
 }
 
-func main() {
-  ctx := context.Background()
-  //token, err := getGithubAPIToken(ctx)
-  //if err != nil {
-  //  panic(err)
-  //}
+// Ensures there is a github read-write client for creating issues, etc
+func (app *App) ensureGithubRWClient() error {
+  if app.gh_client_rw != nil {
+    return nil
+  }
 
-  //ts := oauth2.StaticTokenSource(
-  //  &oauth2.Token{AccessToken: token},
-  //)
-  //tc := oauth2.NewClient(ctx, ts)
-
-  //client := github.NewClient(tc);
-  read_client := github.NewClient(nil);
-
-  issues, _, err := read_client.Issues.ListByRepo(ctx, "w3c", "csswg-drafts", nil)
+  token, err := app.getGithubAPIToken()
   if err != nil {
-    panic(err)
+    return fmt.Errorf("getGithubAPIToken: %v\n", err)
   }
 
-  for _, issue := range issues {
-    fmt.Printf("%d: %s\n", *issue.Number, *issue.Title)
-  }
+  token_source := oauth2.StaticTokenSource(
+      &oauth2.Token{AccessToken: token})
+  token_client := oauth2.NewClient(app.ctx, token_source)
+
+  app.gh_client_rw = github.NewClient(token_client);
+  return nil
 }
 
+// Loads the last time this CF ran from gcp firestore
+func (app *App) loadLastRunTime() (time.Time, error) {
+  client, err := gcpfs.NewClient(app.ctx, gcpProject)
+  if err != nil {
+    return time.Time{}, fmt.Errorf("gcpfs.NewClient: %v\n", err)
+  }
+  defer client.Close()
+
+  docsnap, err := client.Collection(gcpFirestoreCollection).Doc("last_run").Get(app.ctx)
+  if err != nil {
+    return time.Time{}, fmt.Errorf("client.Collection.Doc.Get: %v\n", err)
+  }
+
+  t, ok := docsnap.Data()["time"].(time.Time)
+  if !ok {
+    return time.Time{}, fmt.Errorf("docsnap.DataTo: not ok\n")
+  }
+  return t, nil
+}
+
+// Main run function for the app.
+func (app *App) run() {
+  last_run_time, err := app.loadLastRunTime()
+  if err != nil {
+    log.Printf("loadLastRunTime: %v\n", err)
+    return
+  };
+
+  // TODO: This isn't implemented!
+  // Some random code
+  //  issues, _, err := app..Issues.ListByRepo(ctx, "w3c", "csswg-drafts", nil)
+  //  if err != nil {
+  //    panic(err)
+  //  }
+  //
+  //  for _, issue := range issues {
+  //    fmt.Printf("%d: %s\n", *issue.Number, *issue.Title)
+  //  }
+  //}
+  //
+  fmt.Printf("last run time %s\n", last_run_time.String())
+}
+
+func main() {
+  NewApp(context.Background()).run()
+}
--- csswg-to-local-cf/go.mod
+++ csswg-to-local-cf/go.mod
@@ -3,9 +3,12 @@ module github-resolutions
 go 1.19
 
 require (
-	cloud.google.com/go/compute v1.12.1 // indirect
+	cloud.google.com/go v0.105.0 // indirect
+	cloud.google.com/go/compute v1.13.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.1 // indirect
+	cloud.google.com/go/firestore v1.9.0 // indirect
 	cloud.google.com/go/iam v0.8.0 // indirect
+	cloud.google.com/go/longrunning v0.3.0 // indirect
 	cloud.google.com/go/secretmanager v1.10.0 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
@@ -18,11 +21,14 @@ require (
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/oauth2 v0.4.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/time v0.1.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/api v0.103.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
-	google.golang.org/genproto v0.0.0-20221201164419-0e50fba7f41c // indirect
+	google.golang.org/genproto v0.0.0-20230117162540-28d6b9783ac4 // indirect
 	google.golang.org/grpc v1.51.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 )
