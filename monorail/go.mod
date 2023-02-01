3cb3e536b5d182eea2f0cb62e740e9c5eeeeb9ac
Vladimir Levin <vmpstr@chromium.org>
2023-02-01 11:14:57 -0500

    Added new monorail issues service

--- monorail/go.mod
+++ monorail/go.mod
@@ -1,3 +1,24 @@
 module github.com/chromium-helper/csswg-resolutions/monorail
 
 go 1.19
+
+require (
+	golang.org/x/oauth2 v0.4.0
+	google.golang.org/api v0.109.0
+)
+
+require (
+	cloud.google.com/go/compute v1.14.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.1 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/net v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/text v0.6.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20221227171554-f9683d7f8bef // indirect
+	google.golang.org/grpc v1.51.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- monorail/monorail.go
+++ monorail/monorail.go
@@ -1,9 +1,74 @@
 package monorail
 
 import (
+  "context"
   "fmt"
+  "net/http"
+  "time"
+  "golang.org/x/oauth2"
+  "google.golang.org/api/idtoken"
 )
 
-func Hello() {
-  fmt.Printf("hello\n");
+type IssuesService struct {
+  Token *oauth2.Token
+  HttpClient *http.Client
+  ApiBase string
+}
+
+func contains(needle string, haystack []string) bool {
+  for _, candidate := range haystack {
+    if needle == candidate {
+      return true
+    }
+  }
+  return false
+}
+
+func createIdToken(ctx context.Context, audience string, idtoken_opts idtoken.ClientOption) (*oauth2.TokenSource, *oauth2.Token, error) {
+  token_source, err := idtoken.NewTokenSource(ctx, audience, idtoken_opts)
+	if err != nil {
+		return nil, nil, fmt.Errorf("idtoken.NewTokenSource: %v", err)
+	}
+
+	token, err := token_source.Token()
+	if err != nil {
+		return nil, nil, fmt.Errorf("token_source.Token: %v\n", err)
+	}
+  return &token_source, token, nil;
+}
+
+func createHttpClient(token_source *oauth2.TokenSource) (*http.Client, error) {
+  transport := &oauth2.Transport{
+    Source: *token_source,
+    Base: http.DefaultTransport,
+  }
+  return &http.Client{
+    Transport: transport,
+    Timeout: 1 * time.Minute,
+  }, nil
+}
+
+func NewIssuesService(ctx context.Context, target string, idtoken_opts idtoken.ClientOption) (*IssuesService, error) {
+  if !contains(target, []string{"prod", "dev", "staging"}) {
+    return nil, fmt.Errorf("target must be one of prod, dev, staging\n")
+  }
+
+  audience := fmt.Sprintf("https://monorail-%s.appspot.com", target)
+  api_base := fmt.Sprintf("https://api-dot-monorail-%s.appspot.com/prpc/", target)
+
+  token_source, token, err := createIdToken(ctx, audience, idtoken_opts)
+  if err != nil {
+    return nil, fmt.Errorf("createIdToken: %v\n", err)
+  }
+
+  http_client, err := createHttpClient(token_source)
+  if err != nil {
+    return nil, fmt.Errorf("createHttpClient: %v\n", err)
+  }
+
+	return &IssuesService{
+    Token: token,
+    HttpClient: http_client,
+    ApiBase: api_base,
+	}, nil
 }
